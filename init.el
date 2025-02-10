21504a8b8be8deb8ebbc5b225b6d4719f774ea22
abel7252 <abel7252@outlook.com>
2025-02-10 10:22:41 +0800

    调整了一些bug

--- init.el
+++ init.el
@@ -144,7 +144,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +preview +cdlatex +latexmk +latex)             ; writing papers in Emacs has never been so fun
+       (latex +preview +cdlatex +latexmk)             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
