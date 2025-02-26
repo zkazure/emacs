d5c0ff1000a795445468dc97dcf63894731145df
abel7252 <abel7252@outlook.com>
2025-02-26 10:46:46 +0800

    some error of latex preview automatically File mode specification error: (error File "/tmp/orgtexPh3Agl.xdv" wasn’t produced Please adjust ‘dvisvgm’ part of ‘org-preview-latex-process-alist’.) Undo-Fu-Session can not recover undo data: Invalid read syntax: "#", 2, 97

--- config.el
+++ config.el
@@ -97,31 +97,31 @@
   (setq org-directory "/mnt/d/Personal/org/")
   (setq org-startup-folded t)
   (setq org-log-done 'time))
-(after! org
-  (setq org-latex-packages-alist
-    '(("fontset=SimSun,UTF8" "ctex" t)
-      ("" "amsmath" t)))
-  (setq org-export-with-smart-quotes t)
-  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.2))
-  (setq org-preview-latex-default-process 'dvisvgm) ;; dvisvgm
-  (setq org-startup-with-latex-preview t)
-  (setq org-preview-latex-process-alist
-        '((dvisvgm :programs
-                ("xelatex" "dvisvgm")
-                :description "xdv > svg" :message "you need to install the programs: xelatex and dvisvgm." :use-xcolor t :image-input-type "xdv" :image-output-type "svg" :image-size-adjust
-                (1.7 . 1.5)
-                :latex-compiler
-                ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-                :image-converter
-                ("dvisvgm %f -n -b min -c %S -o %O"))
-        (imagemagick :programs
-                ("xelatex" "convert")
-                :description "pdf > png" :message "you need to install the programs: xelatex and imagemagick." :use-xcolor t :image-input-type "pdf" :image-output-type "png" :image-size-adjust
-                (1.0 . 1.0)
-                :latex-compiler
-                ("xelatex -interaction nonstopmode -output-directory %o %f")
-                :image-converter
-                ("convert -density %D -trim -antialias %f -quality 100 %O")))))
+;; (after! org
+;;   (setq org-latex-packages-alist
+;;     '(("fontset=SimSun,UTF8" "ctex" t)
+;;       ("" "amsmath" t)))
+;;   (setq org-export-with-smart-quotes t)
+;;   (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.2))
+;;   (setq org-preview-latex-default-process 'dvisvgm) ;; dvisvgm
+;;   (setq org-startup-with-latex-preview t)
+;;   (setq org-preview-latex-process-alist
+;;         '((dvisvgm :programs
+;;                 ("xelatex" "dvisvgm")
+;;                 :description "xdv > svg" :message "you need to install the programs: xelatex and dvisvgm." :use-xcolor t :image-input-type "xdv" :image-output-type "svg" :image-size-adjust
+;;                 (1.7 . 1.5)
+;;                 :latex-compiler
+;;                 ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+;;                 :image-converter
+;;                 ("dvisvgm %f -n -b min -c %S -o %O"))
+;;         (imagemagick :programs
+;;                 ("xelatex" "convert")
+;;                 :description "pdf > png" :message "you need to install the programs: xelatex and imagemagick." :use-xcolor t :image-input-type "pdf" :image-output-type "png" :image-size-adjust
+;;                 (1.0 . 1.0)
+;;                 :latex-compiler
+;;                 ("xelatex -interaction nonstopmode -output-directory %o %f")
+;;                 :image-converter
+;;                 ("convert -density %D -trim -antialias %f -quality 100 %O")))))
 
 (map! :leader
       :desc "Org Agenda"
